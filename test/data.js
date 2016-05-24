ffb9aea96857af8e305d7f5298589cb970437cba
Soreine <soreine.plume@gmail.com>
2016-05-24 15:14:31 +0200

    Block-level diffing

---
+++ test/data.js
@@ -0,0 +1,4 @@
+module.exports = {
+    text1: 'Hello, this is a first block.\n\nThis is second block.\n\nHere is a third block.',
+    text2: 'Hello, this is a splitted,\n\nfirst block.\n\nThis is second modified block.\n\nHere is a third block.'
+};
